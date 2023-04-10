4d649530c371ab9433c5f5633e594a74648b3543
aipilotbyjd <128741012+aipilotbyjd@users.noreply.github.com>
2023-04-10 06:08:48 +0000

    qwe

--- app/index.js
+++ app/index.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Card } from "react-native-paper";
 
 const BusinessNameGenrator = () => {
     return (
-        <View>
-            <Text>index</Text>
-        </View>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <View style={styles.cardcontainer}>
+                    <Card style={styles.card}>
+                        <Card.Content>
+                            <Text>Business Name Generator</Text>
+                        </Card.Content>
+                    </Card>
+                    <Card style={styles.smallcard}>
+                        <Card.Content>
+                            <Text>Business Name Generator</Text>
+                        </Card.Content>
+                    </Card>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
 export default BusinessNameGenrator
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+    },
+    cardcontainer: {
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        elevation: 5,
+    },
+    card: {
+        margin: 10,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        elevation: 5,
+    },
+})
